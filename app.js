1421bd564644f6528e621de1d1727431cbb8645f
Volkov Slava <volkov.slava@hotmail.com>
2020-03-18 15:04:53 +0300

    create news app

---
+++ app.js
@@ -0,0 +1,64 @@
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+// Init http module
+const http = customHttp();
+
+//  init selects
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+});
+
+
