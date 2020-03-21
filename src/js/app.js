036582f9d2d3afcbcfdfc00a0a4352c11c51a347
Volkov Slava <volkov.slava@hotmail.com>
2020-03-21 14:17:51 +0300

    added request to the server

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/style.css';
-import UI from './config/ui.config'
-import { validate } from './helpers/validate'
-import { showInputError, removeInputerror } from './views/form'
+import UI from './config/ui.config';
+import { validate } from './helpers/validate';
+import { showInputError, removeInputerror } from './views/form';
+import { login } from './services/auth.service'
 
 const { form, inputEmail, inputPassword} = UI;
 const inputs = [inputEmail, inputPassword];
@@ -17,7 +18,7 @@ inputs.forEach(el => el.addEventListener('focus', () =>removeInputerror(el)));
 
 // Handlers
 
-function onSubmit() {
+async function onSubmit() {
   const isValidForm = inputs.every((el) => {
     const isValidInput = validate(el);
     if (!isValidInput) {
@@ -25,5 +26,14 @@ function onSubmit() {
     }
     return isValidInput;
   });
-  console.log(isValidForm)
+
+  if(!isValidForm) return;
+
+  try {
+    await login(inputEmail.value, inputPassword.value);
+    form.reset();
+    // show success notify
+  } catch (err) {
+    // show error notify
+  }
 }
---
+++ src/js/config/api.config.js
@@ -0,0 +1,7 @@
+const API_ENV = {
+  apiUrl: 'https://mlp-demo.herokuapp.com/api/public',
+};
+
+export default API_ENV;
+
+
